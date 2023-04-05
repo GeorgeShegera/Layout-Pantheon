f1a1a589d8ffd49ffe5d11b63adade6a020328ca
GeorgeShegera <113610588+GeorgeShegera@users.noreply.github.com>
2023-04-05 23:57:42 +0300

    Header Content

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ navs.forEach((nav) => {
     };
   };
 });
+
+/* */
+const headerContent = document.querySelector(".header-content");
+console.log(headerContent);
+
+const burderMenu = document.querySelector(".navbar-toggler");
+console.log(burderMenu);
+
+burderMenu.onclick = () => {
+  const mediaQuery = window.matchMedia("(max-height: 700px)");
+  if (!mediaQuery.matches) {
+    headerContent.style.marginTop = "50px";
+  } else if (burderMenu.classList.contains("collapsed")) {
+    headerContent.style.marginTop = "50px";
+  } else headerContent.style.marginTop = "0px";
+};
