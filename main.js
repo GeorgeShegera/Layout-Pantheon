dd9f28460afad4ce1a639a940006fa3ccd593c12
GeorgeShegera <113610588+GeorgeShegera@users.noreply.github.com>
2023-05-22 00:29:54 +0300

    Membership Tables

--- main.js
+++ main.js
@@ -137,16 +137,11 @@ const gymMembershipsSwiper = new Swiper(".gym-memberships_swiper", {
     el: ".gym-membership-scrollbar",
     draggable: true,
     dragSize: 402,
-    // enabled: true,
   },
 
   breakpoints: {
     950: {
       slidesPerView: 5,
-
-      _scrollbar: {
-        // enabled: false,
-      },
     },
 
     850: {
@@ -175,7 +170,6 @@ const gymMembershipsSwiper = new Swiper(".gym-memberships_swiper", {
       slidesPerView: 1.7,
       scrollbar: {
         dragSize: 70,
-        // enabled: true,
       },
     },
   },
@@ -192,7 +186,23 @@ const membershipPrices = Array.from(
     ".gym-memberships_slide:not(:nth-child(1)) > .gym-membership-item:not(:nth-child(1))"
   )
 );
+
 tableHover(membershipPrices, membershipKinds, membershipDurations);
+
+const membershipByTimeTop = document.querySelectorAll(
+  ".time-memberships_slide:not(:nth-child(1)) > .time-membership-item:nth-child(1)"
+);
+const membershipByTimeLeft = document.querySelectorAll(
+  ".time-memberships_slide:nth-child(1) >  .time-membership-item:not(:nth-child(1))"
+);
+const membershipByTimeCentral = Array.from(
+  document.querySelectorAll(
+    ".time-memberships_slide:not(:nth-child(1)) >  .time-membership-item:not(:nth-child(1))"
+  )
+);
+
+tableHover(membershipByTimeCentral, membershipByTimeLeft, membershipByTimeTop);
+console.log(membershipByTimeTop, membershipByTimeLeft, membershipByTimeCentral);
 
 function tableHover(centralElements, leftElements, topElements) {
   centralElements.forEach((price) => {
@@ -212,3 +222,51 @@ function tableHover(centralElements, leftElements, topElements) {
     };
   });
 }
+
+/* Memberships by time */
+new Swiper(".time-memberships_swiper", {
+  settings: "horizontal",
+  speed: 800,
+  loop: false,
+
+  scrollbar: {
+    el: ".gym-membership-scrollbar",
+    draggable: true,
+    dragSize: 402,
+  },
+
+  breakpoints: {
+    950: {
+      slidesPerView: 5,
+    },
+
+    850: {
+      slidesPerView: 4,
+
+      scrollbar: {
+        dragSize: 402,
+      },
+    },
+
+    576: {
+      slidesPerView: 3,
+      scrollbar: {
+        dragSize: 202,
+      },
+    },
+
+    350: {
+      slidesPerView: 2,
+
+      scrollbar: {
+        dragSize: 102,
+      },
+    },
+    0: {
+      slidesPerView: 1.7,
+      scrollbar: {
+        dragSize: 70,
+      },
+    },
+  },
+});
